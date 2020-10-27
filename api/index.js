036ff604ba367b632ff3ac4c51fc1c4efff449cc
saurmo - Santiago Urrego Morales <santimo0108@gmail.com>
2020-10-27 06:57:49 -0500

    postman collection

--- api/index.js
+++ api/index.js
@@ -15,3 +15,13 @@ const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`API running http://localhost:` + PORT);
 });
+
+eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.
+eyJub21icmUiOiJDYXJvbGluYSIsImFwZWxsaWRvIjoiVXJyZWdvIiwiY29ycmVvIjoiY2Fyb2xpbmFAdWRlbS5lZHUuY28
+iLCJyb2wiOjEsImlhdCI6MTYwMzc5ODA2NSwiZXhwIjoxNjAzODAxNjY1fQ.
+-rA2GIPyUEfsPyTc6X8JiGIJf2L5OiqzN4ElLeQ2Fik
+
+eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.
+eyJub21icmUiOiJDYXJvbGluYSIsImFwZWxsaWRvIjoiVXJyZWdvIiwiY29ycmV
+vIjoiY2Fyb2xpbmFAdWRlbS5lZHUuY28iLCJyb2wiOjEsImlhdCI6MTYwMzc5OTY1NSwiZXhwIjoxNjAzODAzMjU1fQ.
+S6UQY5tCsuUX4f5pBtfpIPg15kPBX-GaiNy7EKAmBxA
