d0263a24463fa581dd2a7a12daf19f0c70467cf4
saurmo - Santiago Urrego Morales <santimo0108@gmail.com>
2020-10-27 06:46:15 -0500

    cors

--- api/index.js
+++ api/index.js
@@ -2,10 +2,12 @@ require("./config");
 const express = require("express");
 const routes = require("./routes/routes");
 const fileUpload = require("express-fileupload");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
 app.use(fileUpload());
+app.use(cors());
 
 app.use(routes);
 
