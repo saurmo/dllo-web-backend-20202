6411862501226af028bd7eb827e896007f00ca67
saurmo - Santiago Urrego Morales <santimo0108@gmail.com>
2020-11-12 06:52:13 -0500

    Config db heroku

--- api/config/index.js
+++ api/config/index.js
@@ -11,9 +11,10 @@ if (process.env.NODE_ENV == "dev") {
   process.env.PORT_DB = 5432;
   process.env.URL_DOCS = `http://localhost:${process.env.PORT}/public/`;
 } else if (process.env.NODE_ENV == "production") {
-  process.env.USER_DB = "";
-  process.env.HOST_DB = "";
-  process.env.DB = "";
-  process.env.PASSWORD_DB = "";
+  process.env.USER_DB = "snhngbaxlxaurw";
+  process.env.HOST_DB = "ec2-3-216-89-250.compute-1.amazonaws.com";
+  process.env.DB = "d2s3e228cu1fnh";
+  process.env.PASSWORD_DB =
+    "dddba8dcbc194ea4d1d64dbd0fecbd080da45fd40ffc62299f33469abe64184a";
   process.env.PORT_DB = 5432;
 }
